867f77280c4abe2810b97da0a099d3a19d153ce7
Regis Leandro Buske <regisleandro@Regis-Leandro-Macs.local>
2024-06-28 17:46:50 -0300

    chore: configured to use AzureOpenAI

--- pkg/chatbot.py
+++ pkg/chatbot.py
@@ -1,9 +1,18 @@
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
+from openai import AzureOpenAI
 
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 import json
+
+load_dotenv()
+
+client =  AzureOpenAI(
+  api_version=os.getenv('AZURE_AP_VERSION'),
+  api_key=os.getenv('AZURE_OPENAI_API_KEY'),
+  azure_deployment=os.getenv('AZURE_DEPLOYMENT_ID')
+)
+
+# import json
 import pandas as pd
 import pkg.rabbit as rabbit_service
 import pkg.mongo as mongo_service
