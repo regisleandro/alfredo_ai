e51c027b92a2ef9d0e0686bb53d4622844a2d8b8
regisleandro <regisleandro@gmail.com>
2025-04-05 18:25:10 -0300

    fix: removed background execution for rocket chat

--- start_services.sh
+++ start_services.sh
@@ -4,5 +4,4 @@
 streamlit run /app/streamlit_app.py &
 
 # Start the FastAPI server in the background
-uvicorn api.main:app --host 0.0.0.0 --port 8000 &
-
+uvicorn api.main:app --host 0.0.0.0 --port 8000
